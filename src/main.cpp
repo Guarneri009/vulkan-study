a508b0e91497075175dcb20d5eb342441fb58a60
Guarneri009 <driftinghide@gmail.com>
2024-12-01 22:41:41 +0900

    Add pickPhysicalDevice function

--- src/main.cpp
+++ src/main.cpp
@@ -90,6 +90,41 @@ private:
         createInstance();
         // This function is called to set debug messaging.
         setupDebugMessenger();
+        pickPhysicalDevice();
+    }
+
+    void pickPhysicalDevice()
+    {
+        VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
+        uint32_t deviceCount = 0;
+        vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr);
+
+        if (deviceCount == 0)
+        {
+            throw std::runtime_error("failed to find GPUs with Vulkan support!");
+        }
+
+        std::vector<VkPhysicalDevice> devices(deviceCount);
+        vkEnumeratePhysicalDevices(instance, &deviceCount, devices.data());
+
+        for (const auto &device : devices)
+        {
+            if (isDeviseSuitable(device))
+            {
+                physicalDevice = device;
+                break;
+            }
+        }
+
+        if (physicalDevice == VK_NULL_HANDLE)
+        {
+            throw std::runtime_error("failed to find a suitable GPU!");
+        }
+    }
+
+    bool isDeviseSuitable(VkPhysicalDevice devise)
+    {
+        return true;
     }
 
     void mainLoop()
